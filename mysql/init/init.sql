cc54d05e2cd347f34e462f6e58637cd78701b5b1
yasu2122yasu <ya2su1shi2@icloud.com>
2023-12-04 17:15:36 +0900

    fix: fix SQL

--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -13,5 +13,23 @@ CREATE TABLE users (
     updated_at DATETIME NOT NULL
 );
 
+CREATE TABLE turns (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    week VARCHAR(255) NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL
+);
+
+CREATE TABLE characters (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+-- テストデータを挿入
 INSERT INTO users (name, email, password, created_at, updated_at)
 VALUES ('John Doe', 'johndoe@example.com', 'secretpassword', '2023-11-29 13:37:27.864', '2023-11-29 13:37:27.864');
