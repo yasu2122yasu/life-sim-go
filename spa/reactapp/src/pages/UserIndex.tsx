a1bf8085e473be2fbdad13c82f683705dd74a14b
yasu2122yasu <ya2su1shi2@icloud.com>
2023-12-14 01:07:26 +0900

    make pull requeset

--- spa/reactapp/src/pages/UserIndex.tsx
+++ spa/reactapp/src/pages/UserIndex.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Header from './../components/Header';
-import SideBar from './../components/SideBar';
 
 interface UserData {
   ID: number;
@@ -13,40 +12,34 @@ interface UserData {
 }
 
 export const UserIndex = () => {
-  const [userId, setUserId] = useState<number>(1);
-  const [userData, setUserData] = useState<UserData | null>(null);
+  const [userData, setUserData] = useState<UserData[]>([]); // ユーザーデータの配列
 
   useEffect(() => {
     axios
-      .get(`http://localhost:8080/users/1`)
+      .get('http://localhost:8080/users')
       .then((response) => {
-        setUserData(response.data);
+        setUserData(response.data); // レスポンスデータを配列として設定
       })
       .catch((error) => {
         console.error('Error fetching data: ', error);
       });
-  }, [userId]); // userIdが変更されたときにuseEffectを再実行
+  }, []); // コンポーネントがマウントされたときに一度だけ実行
 
-  const handleUserIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUserId(Number(event.target.value)); // 入力された値を数値に変換してuserIdを更新
-  };
-
-  if (!userData) return <div>Loading...</div>;
+  if (userData.length === 0) return <div>Loading...</div>;
 
   return (
     <div>
       <Header title="User Information" />
-      <div style={{ float: 'right' }} className="sidebar">
-        {/* Add className to adjust SideBar */}
-        <SideBar />
-      </div>
       <h1>User Data</h1>
-      <input type="number" value={userId} onChange={handleUserIdChange} placeholder="Enter User ID" />
-      <p>ID: {userData.ID}</p>
-      <p>Name: {userData.Name}</p>
-      <p>Email: {userData.Email}</p>
-      <p>Created At: {userData.CreatedAt}</p>
-      <p>Updated At: {userData.UpdatedAt}</p>
+      {userData.map((user) => (
+        <div key={user.ID}>
+          <p>ID: {user.ID}</p>
+          <p>Name: {user.Name}</p>
+          <p>Email: {user.Email}</p>
+          <p>Created At: {user.CreatedAt}</p>
+          <p>Updated At: {user.UpdatedAt}</p>
+        </div>
+      ))}
     </div>
   );
 };
